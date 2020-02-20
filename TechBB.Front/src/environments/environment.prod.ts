fa2168f1307622008eb0b7758ed6fde38d233d55
Dusan Malusev <dusan.998@outlook.com>
2020-02-20 18:37:50 +0100

    Removing environment stuff - protecting google maps api key - no storage in repo git add .!
    Signed-off-by: Dusan Malusev <dusan.998@outlook.com>

--- TechBB.Front/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import { Env } from "./env";
-
-export const environment: Env = {
-  production: true,
-  api: "http://api.techbb.app/api",
-  googleMaps: "AIzaSyAaRY7d2lzB-2ynj88AsZ2BcwHWLE9mIK8"
-};
--- TechBB.Front/src/environments/environment.ts
+++ TechBB.Front/src/environments/environment.ts
@@ -2,8 +2,8 @@ import { Env } from "./env";
 
 export const environment: Env = {
   production: false,
-  api: "https://api.techbb.app/api",
-  googleMaps: "AIzaSyAaRY7d2lzB-2ynj88AsZ2BcwHWLE9mIK8"
+  api: "API URL",
+  googleMaps: "YOUR API KEY"
 };
 
-import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import "zone.js/dist/zone-error";
